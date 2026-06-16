5db35eab336451510404e187637a30c560a2cc11
James Bray <jamesmbray@outlook.com>
2026-06-16 13:52:40 +0800

    fixing links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://jamesbrayy.github.io',
   base: '/portfolio',
-  integrations: [tailwind()],
   devToolbar: {
     enabled: false
   }
