b78d2dacd2fa9ac0fccb2bbee847083d3e2f5a49
James Bray <jamesmbray@outlook.com>
2026-06-16 13:37:44 +0800

    hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: 'https://jamesbrayy.github.io',
+  base: '/portfolio',
   devToolbar: {
     enabled: false
   }
